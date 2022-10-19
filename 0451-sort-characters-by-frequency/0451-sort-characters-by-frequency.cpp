02c4f129ed2a17446b1f8666c46d8d4eade00e20
theFireWraith <teja.surya6@gmail.com>
2022-10-18 21:05:39 -0700

    Time: 62 ms (7.87%), Space: 8 MB (89.94%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int> freq(256, 0);
+        int freq[256] = {};
         for(auto c: s) 
             freq[c]++;
         sort(s.begin(), s.end(), [&](char a , char b){
