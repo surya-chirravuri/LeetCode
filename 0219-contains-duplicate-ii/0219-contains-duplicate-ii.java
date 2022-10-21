eee4373f5bb46fdf88f620f2b542c522bcc53291
theFireWraith <teja.surya6@gmail.com>
2022-10-20 19:32:36 -0700

    Time: 20 ms (88.22%), Space: 50 MB (99.41%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             if(indexMap.containsKey(nums[i])) {
                 int diff = i - indexMap.get(nums[i]);
-                if(diff <= k)
-                    return true;
+                if(diff <= k) return true;
             }
             indexMap.put(nums[i], i);
         }
