f1d7d075ac485944be7838f7832372412ec93741
theFireWraith <teja.surya6@gmail.com>
2023-08-22 21:27:24 -0700

    Time: 15 ms (44.83%), Space: 47.3 MB (89.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> frequency = new HashMap<Integer, Integer>();
-        for(int num: nums) {
-            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
         }
         
-        Queue<Integer> minHeap = new PriorityQueue<Integer>((Integer a, Integer b ) -> frequency.get(a) - frequency.get(b));
-        
-        for(int num: frequency.keySet()) {
-            minHeap.add(num);
-            if(minHeap.size() > k) minHeap.poll();
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
         }
-        
-        int [] result = new int[k];
-        for(int i = k-1; i >= 0; i--) {
-            result[i] = minHeap.poll();
+
+        // init heap 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>(
+            (n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+          heap.add(n);
+          if (heap.size() > k) heap.poll();    
         }
-        
-        return result;
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
     }
 }
