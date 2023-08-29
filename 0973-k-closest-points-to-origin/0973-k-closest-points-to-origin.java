cf566b369cef3bca766fd5f0c08b81b59e3c47c4
theFireWraith <teja.surya6@gmail.com>
2023-08-28 21:35:13 -0700

    Time: 26 ms (77.69%), Space: 46.4 MB (93.85%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,25 +3,25 @@ class Solution {
         return point[1] * point[1] + point[0] * point[0];
     }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<List<Integer>> maxHeap = new PriorityQueue<List<Integer>>((List<Integer> a, List<Integer> b) -> {
-            return b.get(1) - a.get(1);
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((int[] a, int[] b) -> {
+            return distanceFromOrigin(b) - distanceFromOrigin(a);
         });
         int index = 0;
         for(int[] point : points) {
             if(maxHeap.size() == k) {
-                if(maxHeap.peek().get(1) > distanceFromOrigin(point)) {
+                if(distanceFromOrigin(maxHeap.peek()) > distanceFromOrigin(point)) {
                     maxHeap.poll();
-                    maxHeap.offer(Arrays.asList(index, distanceFromOrigin(point)));
+                    maxHeap.offer(point);
                 }
             } else {
-                maxHeap.offer(Arrays.asList(index, distanceFromOrigin(point)));
+                maxHeap.offer(point);
             }
-            index++;
+            // index++;
         }
         int[][] res = new int[k][2];
         while(--k >= 0) {
-            index = maxHeap.poll().get(0);
-            res[k] = points[index];
+            // index = maxHeap.poll().get(0);
+            res[k] = maxHeap.poll();
         }
         
         return res;
