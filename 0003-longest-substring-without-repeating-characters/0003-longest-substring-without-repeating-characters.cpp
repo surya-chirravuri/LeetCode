084d4a4f9863a7657962cc0168f76c0a07205c69
theFireWraith <teja.surya6@gmail.com>
2023-09-22 20:55:16 -0700

    Time: 10 ms (76.83%), Space: 8.7 MB (52.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,19 +4,10 @@ public:
         unordered_map<char,int> frequency;
         int start = 0, end = 0;
         int len = 0;
-        // cout<<frequency.size();
         for(end = 0; end < s.size(); end++) {
             frequency[s[end]]++;
-            // if(frequency[s[end]] == 1) {
-            //     continue;
-            // }
-            // cout<<frequency.size()<<" "<<start<<" "<<end<<endl;
             while(frequency[s[end]] > 1) {
-                // if(frequency[s[start]]-- == 0) {
-                //     frequency.erase(s[start]);
-                // }
                 frequency[s[start++]]--;
-               // start++;
             }
             len = max(len, end-start+1);
         }
