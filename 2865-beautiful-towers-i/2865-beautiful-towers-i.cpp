6a4dba2b19fe7dabb256d42f61d6bc64672d2ed2
theFireWraith <teja.surya6@gmail.com>
2023-09-23 22:21:42 -0700

    Time: 131 ms (10.00%), Space: 98.4 MB (10.00%) - LeetHub

---
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maximumSumOfHeights(vector<int>& maxHeights) {
+        long long maxSum = 0;
+        for(int peakIndex = 0; peakIndex < maxHeights.size(); peakIndex++ ) {
+            long long sum = 0;
+            vector<int> heights(maxHeights.size());
+            heights[peakIndex] = maxHeights[peakIndex];
+            for(int i = peakIndex-1; i >= 0; i--) {
+                heights[i] = min(maxHeights[i], heights[i+1]);
+            }
+            for(int i = peakIndex + 1; i < maxHeights.size() ; i++) {
+                // heights[i] = maxHeights[i];
+//                
+                heights[i] = min(heights[i-1], maxHeights[i]);
+                // }
+            }
+            for(auto h: heights){
+                // cout<<h<<" ";
+                sum += h;
+            }
+            // cout<<endl;
+            maxSum = max(sum, maxSum);
+        }
+        return maxSum;
+    }
+};
