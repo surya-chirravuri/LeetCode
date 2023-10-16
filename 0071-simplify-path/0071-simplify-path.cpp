d4893847d17477c6524294fa00a853ef891de7fd
theFireWraith <teja.surya6@gmail.com>
2023-10-15 19:32:52 -0700

    Time: 2 ms (92.64%), Space: 7.9 MB (84.90%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dequeAppend(deque<string> &dirs, string dir) {
+        // cout<<dir<<endl;
+        if(dir.empty() || dir == ".") return;
+        if(dir == "..") {
+            if(!dirs.empty()) dirs.pop_back();
+        }
+        else dirs.push_back(dir);
+    }
+    
+    string simplifyPath(string path) {
+        string res = "/";
+        deque<string> dirs;
+        string dir = "";
+        for(int i = 0; i < path.size(); i++) {
+            if(path[i] == '/') {
+                dequeAppend(dirs, dir);
+                dir = "";
+            } else {
+                dir += path[i];
+                if(i == path.size()-1) dequeAppend(dirs,dir);
+            } 
+        }
+        int i = 0;
+        for(auto dir: dirs) {
+            res += dir;
+            if(i != dirs.size()-1)
+                res += "/";
+            i++;
+        }
+        return  res;
+    }
+};
