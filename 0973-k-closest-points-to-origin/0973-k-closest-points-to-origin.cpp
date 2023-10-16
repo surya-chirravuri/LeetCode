58644b90f3ad6bc1562899cd8c72332e96cef2c4
theFireWraith <teja.surya6@gmail.com>
2023-10-16 14:35:39 -0700

    Time: 292 ms (32.23%), Space: 72 MB (47.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        auto cmp = [](vector<int> a, vector<int> b) {
-            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
-        vector<vector<int>> res(k);
-        for(auto point: points) {
+        priority_queue<vector<int>, vector<vector<int>>, compare> pq;
+        for (vector<int>& point : points) {
             pq.push(point);
-            if(pq.size() > k)
+            if (pq.size() > k) {
                 pq.pop();
+            }
         }
-        while(!pq.empty()) {
-            res[--k] = pq.top();
+        vector<vector<int>> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
             pq.pop();
         }
-        return res;
+        return ans;
     }
+private:
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
 };
